8774c9dd726c02411b05bccb568e1d58f9d9cfe4
Toney G Jolly <85247469+toneygjolly@users.noreply.github.com>
2023-08-09 19:29:49 +0530

    Update studentselection.java

--- projects/studentselection.java
+++ projects/studentselection.java
@@ -1,40 +1,45 @@
 import java.util.Scanner;
 
-
-public class studentselection {
-    public static void main(String[] args)
-    {
-        System.out.println("Enter the number of students ");
+public class vaccine{
+    public static void main(String[] args){
+        System.out.println("$$$For getting the vaccine Age will above 15 $$$$$");
+        System.out.println("enter the number of students");
         Scanner sc=new Scanner(System.in);
         int numStudent=sc.nextInt();
-        String[] student=new String[numStudent];
-        int[]age = new int[numStudent];
-        Scanner scName =new Scanner(System.in);
-        Scanner scAge =new Scanner(System.in);
-        for(int i=0;i<=numStudent;i++)
-        {
-        System.out.println("Enter name:");
-        student[i] = scName.nextLine();
-        System.out.println("Enter age");
-        age[i]=scAge.nextInt();
-
-        }
-        for(int i=0;i<numStudent;i++)
-        {
-        if(age[i]>=15)
-        {
-                System.out.println(student[i]+"can recive the vacine");
-                System.out.println("The student is now"+age[i]);
-        }
-
-        }
-        sc.close();
-        scName.close();
-        scAge.close();
-
-
+        
             
+            String []name=new String[numStudent];
+            Scanner scname=new Scanner(System.in);
+            
+           int[]age= new int[numStudent];
+           Scanner scage=new Scanner(System.in);
+            
+        
+        for(int i=0;i<numStudent;i++){
+        System.out.println("Enter the name");
+        name[i]=sc.next();
+        System.out.println("Enter the age");
+        age[i]=sc.nextInt();
+        
+        
+        }
+ for(int i=0;i<numStudent;i++){
     
-    }
+ if(age[i]>15){
 
+ System.out.println("Eligible for vaccine::"+name[i]+", Age :"+age[i]);
+ 
+ }
+ else{
+    System.out.println("Not Eligible for vacine::"+name[i]+", Age :"+age[i]);
+ } 
 }
+sc.close();
+scname.close();
+scage.close();
+
+           
+        }
+        
+
+    } 
