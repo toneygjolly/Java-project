c75284f4673e16c062a725a3de45fc2c51f98752
Toney G Jolly <85247469+toneygjolly@users.noreply.github.com>
2023-08-10 14:13:01 +0530

    Update budget.java

--- projects/budget.java
+++ projects/budget.java
@@ -1,34 +1,30 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class budget {
+public class budjet{
     public static void main(String[] args){
-        System.out.println("Enter your budget amount");
-        Scanner scan=new Scanner(System.in);
-        Double budget=scan.nextDouble();
-        ArrayList<Double>amount=new ArrayList<Double>();
-        System.out.println("Enter the budget");
-        int i=0;
-        double sum=0;
-        do{
-         System.out.println("enter the expense"+(i+1)+":");
-                   Double value=scan.nextDouble();
-                   amount.add(value);
-                   sum+=amount.get(i);
+        System.out.println("Enter the Budget amount::");
+        Scanner sc=new Scanner(System.in);
+        double budget=sc.nextDouble();
+        double totalExpenses=0;
+        while(budget>=totalExpenses){
+            System.out.println("Enter the expense name");
+            String name=sc.next();
+            System.out.println("enter the expenses Rs:");
+             double expense =sc.nextDouble();
+             totalExpenses +=expense;
+            
+             if(totalExpenses<=budget){
+                System.out.println("Remaining amount"+(budget-totalExpenses));
+             }
+             else{
+                System.out.println("Budget exceeded!");
+                 
+                break;
+             }
+            System.out.println("The expense :"+name+" Rs:" +expense);
         }
-        while(sum<budget);
-        //scan.close();
-        System.out.println("value"+sum+"the value is over budget amount");
-        if(sum<budget){
-        System.out.println("close");
-        System.out.println("value"+sum+"the value is over budget amount");
+   
+    }
+}
 
-       }
-       
-        
-
-                  }              
-        }
-
-    
 
